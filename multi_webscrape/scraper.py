750c4e7c34924ff46abdc90bbe5fd99604bce57b
Matthew Chung <mcchung@ucdavis.edu>
2018-09-20 11:31:57 -0700

    Update scraper.py

--- multi_webscrape/scraper.py
+++ multi_webscrape/scraper.py
@@ -3,7 +3,7 @@ from requests_html import HTMLSession
 session = HTMLSession()
 
 # Read an Excel table into a pandas DataFrame
-df = pd.read_excel('cryptofundurls.xlsx', sheet_name='Sheet1', index_col=0)
+df = pd.read_excel('cryptofundurls.xlsx', sheet_name='Sheet1')
 
 # Convert the DataFrame to a dictionary
 list_of_dicts = df.to_dict(orient='records')
@@ -11,11 +11,17 @@ list_of_dicts = df.to_dict(orient='records')
 
 def parse_doc():
     """Set response object. Display website url and additional links."""
-    for char in list_of_dicts:
-            response = session.get(char['Website URL'])
-            print(response.headers)
-            print(response.url, response.html.links)
+    for item in list_of_dicts: # 23 times
+        r = session.get(item['Website URL'])
+        if 'Last-Modified' in r.headers: # 4 times
+            for key in range(len(list_of_dicts)): # 23 times
+                list_of_dicts[key].update({'Last-Modified': r.headers['Last-Modified']})
+                print(list_of_dicts[key])
+
 
 
 parse_doc()
 # From within the <ul class = 'sb-rss-feed' </ul> new <li> ... </li> then "Latest" is updated.
+
+#         else:
+#             list_of_dicts[key].update({'Last-Modified': 'N/A'})
