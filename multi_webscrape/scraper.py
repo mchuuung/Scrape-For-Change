ee5396ce1bc607788ce6ce8170c1fbf55d987cc4
Matthew Chung <mcchung@ucdavis.edu>
2018-09-21 12:18:54 -0700

    Update scraper.py

--- multi_webscrape/scraper.py
+++ multi_webscrape/scraper.py
@@ -1,27 +1,46 @@
 import pandas as pd
-from requests_html import HTMLSession
-session = HTMLSession()
+import requests as req
+import numpy as np
+import json
+from openpyxl import Workbook
+from openpyxl.utils.dataframe import dataframe_to_rows
 
-# Read an Excel table into a pandas DataFrame
+
+# Read an Excel file into a pandas DataFrame
 df = pd.read_excel('cryptofundurls.xlsx', sheet_name='Sheet1')
-
-# Convert the DataFrame to a dictionary
-list_of_dicts = df.to_dict(orient='records')
+df['Last-Modified'] = np.nan  # Sets all rows as empty
 
 
 def parse_doc():
     """Set response object. Display website url and additional links."""
-    for item in list_of_dicts: # 23 times
-        r = session.get(item['Website URL'])
-        if 'Last-Modified' in r.headers: # 4 times
-            for key in range(len(list_of_dicts)): # 23 times
-                list_of_dicts[key].update({'Last-Modified': r.headers['Last-Modified']})
-                print(list_of_dicts[key])
+    for value in df.get('Website URL'):  # Obtain all values in website column
+        r = req.head(value)
+        rh = dict(r.headers)
+        if 'Last-Modified' in r.headers:
+            df.set_index('Last-Modified')
+            # df['Last-Modified'].merge((r.headers['Last-Modified'].to_frame()), left_index=True) # Sets entire column to last date
+            print(rh['Last-Modified']) # prints as dictionary
+            df['Last-Modified'] = pd.DataFrame({'Last-Modified': rh})
+
+        print(df)
+
+            # print(json.dumps(rh)) # dumps takes an object and produces a string
 
 
+    # print(df)
+    # print(r.headers['Last-Modified'])
 
 parse_doc()
-# From within the <ul class = 'sb-rss-feed' </ul> new <li> ... </li> then "Latest" is updated.
 
-#         else:
-#             list_of_dicts[key].update({'Last-Modified': 'N/A'})
+
+# To convert a dataframe into a worksheet highlighting the header and index:
+wb = Workbook()
+ws = wb.active
+
+for r in dataframe_to_rows(df, index=True, header=True):
+    ws.append(r)
+
+for cell in ws['A'] + ws[1]:
+    cell.style = 'Pandas'
+
+wb.save("pandas_openpyxl.xlsx")
