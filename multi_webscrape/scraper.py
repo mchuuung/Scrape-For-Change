fd0c900777d0b7bb3eb77f4e587dffe4560e6983
Matthew Chung <mcchung@ucdavis.edu>
2018-09-21 20:10:13 -0700

    Update scraper.py

--- multi_webscrape/scraper.py
+++ multi_webscrape/scraper.py
@@ -1,46 +1,41 @@
 import pandas as pd
 import requests as req
 import numpy as np
-import json
-from openpyxl import Workbook
-from openpyxl.utils.dataframe import dataframe_to_rows
-
+import openpyxl
 
 # Read an Excel file into a pandas DataFrame
 df = pd.read_excel('cryptofundurls.xlsx', sheet_name='Sheet1')
 df['Last-Modified'] = np.nan  # Sets all rows as empty
 
 
-def parse_doc():
-    """Set response object. Display website url and additional links."""
-    for value in df.get('Website URL'):  # Obtain all values in website column
-        r = req.head(value)
-        rh = dict(r.headers)
-        if 'Last-Modified' in r.headers:
-            df.set_index('Last-Modified')
-            # df['Last-Modified'].merge((r.headers['Last-Modified'].to_frame()), left_index=True) # Sets entire column to last date
-            print(rh['Last-Modified']) # prints as dictionary
-            df['Last-Modified'] = pd.DataFrame({'Last-Modified': rh})
+for value in df['Website URL']:  # Obtain all values in website column
+    r = req.get(value) # Response object
+    if 'Last-Modified' in r.headers: # Checks headers for string
+        contents = r.headers['Last-Modified'] # Sets 'contents' as modified date
+        print(contents)
+        # print(type(contents)) # Type string
+        # print(type(r.headers)) # requests.structures.CaseInsensitiveDict
+d = []
+for p in game.players.passing():
+    d.append({'Player': p, 'Team': p.team, 'Passer Rating':
+        p.passer_rating()})
 
-        print(df)
-
-            # print(json.dumps(rh)) # dumps takes an object and produces a string
+pd.DataFrame(d)
 
 
-    # print(df)
-    # print(r.headers['Last-Modified'])
 
-parse_doc()
+
+
 
 
 # To convert a dataframe into a worksheet highlighting the header and index:
-wb = Workbook()
-ws = wb.active
-
-for r in dataframe_to_rows(df, index=True, header=True):
-    ws.append(r)
-
-for cell in ws['A'] + ws[1]:
-    cell.style = 'Pandas'
-
-wb.save("pandas_openpyxl.xlsx")
+# wb = Workbook()
+# ws = wb.active
+#
+# for r in dataframe_to_rows(df, index=True, header=True):
+#     ws.append(r)
+#
+# for cell in ws['A'] + ws[1]:
+#     cell.style = 'Pandas'
+#
+# wb.save("pandas_openpyxl.xlsx")
