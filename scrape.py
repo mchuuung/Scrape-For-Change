b6553af29a63f4a5389d10a61dbcec410c6f8e45
Matthew Chung <mcchung@ucdavis.edu>
2018-10-09 18:02:06 -0700

    Update scrape.py
    date error changing even when hash remains same

--- scrape.py
+++ scrape.py
@@ -31,6 +31,9 @@ class ParseFile:
         self.master_hash_list = []
         self.final_list = []
         self.merge_final = pd.DataFrame
+        self.olddate_list = []
+        self.check_hash_time = []
+        self.master_hash_time = []
 
     def set_file(self, file):
         """Sets file to be parsed"""
@@ -58,6 +61,7 @@ class ParseFile:
                     self.hash_values.append(h)
                     self.hash_url.append(url)
                     self.current_date.append(datetime.datetime.now())
+        return self.header_dates, self.header_url, self.hash_values, self.hash_url, self.current_date
 
     def create_hash_df(self):
         self.hash_df = pd.DataFrame({'Website URL': self.hash_url, 'Last-Modified': self.current_date, "Hash-Value": self.hash_values})  # date of last modified and time
@@ -79,7 +83,7 @@ class ParseFile:
 
 
     def final_merge(self):
-        self.merge_final = self.merge_df2.astype(str).merge(self.checked_hash_df.astype(str), on='Hash-Value', how='left', suffixes=('_', ''))
+        self.merge_final = self.merge_df2.astype(str).merge(self.checked_hash_df.astype(str), on=['Hash-Value', 'Last-Modified'], how='left', suffixes=('_', ''))
         # self.merge_final = self.merge_final.drop(columns=["Last-Modified_"])
         self.merge_final = self.merge_final.astype(str)
         return self.merge_final
@@ -90,12 +94,12 @@ class ParseFile:
             wb = Workbook()
             ws = wb.active
 
-            for r in dataframe_to_rows(self.use_df, index=True, header=True):
+            for r in dataframe_to_rows(self.use_df, index=True, header=True ):
                 ws.append(r)
 
-            for cell in ws['A'] + ws[1]:
+            for cell in ws['A'] + ws[1]: # Bolds header and index
                 cell.style = 'Pandas'
-
+            ws.delete_rows(2, 1) # empty row that appends
             wb.save(self.save_to_file)
 
     def check_hash(self):
@@ -103,57 +107,45 @@ class ParseFile:
         Also different hash values occuring, but last mod becoming 'nan' as opposed to current date. """
         self.hash_frame = pd.read_excel(self.file) # Read data into pandas DF
         for hash_val in self.hash_frame['Hash-Value']:  # Obtain all hashes in Hash-Value column
-            if not hash_val != hash_val: # Where hash-value is given
-                if self.file == "check3.xlsx":
+            if not hash_val != hash_val: # Where hash-value is given (any valid value)
+                if self.file == "checkAgainst.xlsx":
                     self.check_hash_list.append(hash_val) # append all hash values
-                elif self.file == "MasterFile.xlsx":
+                elif self.file == "CheckV1.xlsx":
                     self.master_hash_list.append(hash_val)
         return self.check_hash_list, self.master_hash_list
-        # Store lists and compare them to set conditions
 
     def compare_list(self):
+
         for i in self.check_hash_list:
-            for x in self.master_hash_list:
-                if i == x:
-                    self.final_list.append(x)
-        self.checked_hash_df = pd.DataFrame({"Hash-Value": self.final_list})  # Matching hash values from both files
+            if i in self.master_hash_list:
+                self.final_list.append(i) #+ matching hash values from "MasterFile" and "checkAgainst"
+            else:
+                self.olddate_list.append(datetime.datetime.now())
+        self.checked_hash_df = pd.DataFrame({"Last-Modified": self.olddate_list, "Hash-Value": self.final_list})  # Matching hash values from both files
         return self.checked_hash_df
-
-
-        #         self.check_hash_against.append(hash_val)
-        #         self.check_hash_date.append(datetime.datetime.now())
-        #
-        # self.checked_hash_df = pd.DataFrame({'Last-Modified': self.check_hash_date,"Hash-Value": self.check_hash_against})
-        # return self.checked_hash_df
 
 
 
 def main():
-    file = ParseFile("MasterFile.xlsx")  # instantiate class instance
+    file = ParseFile("MasterFileTesting.xlsx")  # instantiate class instance
+    file.set_df()
+    file.save_wb('CheckV1.xlsx', file.current_df) # Store current master file
+    file.set_file("MasterFileTesting.xlsx")
     file.get_file()
     file.create_hash_df()
     file.create_header_df()
     file.combine_df()
     file.combine_df2()
-    file.save_wb("check3.xlsx", file.merge_df2) # master unaffected till here
-    file.set_file("MasterFile.xlsx") # current working file
-    file.set_file("check3.xlsx")
+    file.save_wb("checkAgainst.xlsx", file.merge_df2) # master unaffected till here
+    file.set_file("checkAgainst.xlsx")
     file.check_hash() # Checks current hash value from MasterFile
-    file.set_file("MasterFile.xlsx")
+    file.set_file("CheckV1.xlsx")
     file.check_hash()
-    file.compare_list()
+    file.compare_list() #initial run will be empty
     file.final_merge()
-    file.save_wb('OfficialList.xlsx', file.merge_final)
-
-    # file.merge_df3() # Merges checkfile with masterfile into official list
-    # file.save_wb('OfficialList.xlsx', file.final_df)
+    file.save_wb('MasterFileTesting.xlsx', file.merge_final)
 
 
-
-
-    #file.save_wb()
-    #file.set_file("cryptofundurls.xlsx") # Checks a new file (use for comparision of hash)
-    #print(file.get_file()) # Checks output
-
+"""Taking the time from check3 and making it the official end time. not wanted"""
 """Add the last modified time for new appended hash values into officiali list and done."""
 main()
